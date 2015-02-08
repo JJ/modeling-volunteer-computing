35f24b20c2290ab5473580d2209bd937f85ba8f7
JJ Merelo <jjmerelo@gmail.com>
2015-02-08 18:33:44 +0100

    Adding graphs with results

---
+++ data/IPs.R
@@ -0,0 +1,2 @@
+
+number.ips.2 <- c(length(traps.ips.1$IP), length(traps.ips.2$IP), length(traps.ips.3$IP), length(traps.ips.4$IP),length(traps.ips.5$IP), length(traps.ips.6$IP), length(traps.ips.7$IP),length(traps.ips.8$IP), length(traps.ips.8$IP), length(traps.ips.10$IP))
