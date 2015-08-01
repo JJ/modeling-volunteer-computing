cf4db0ed20d451d97971ea6e247421023458398a
JJ Merelo <jjmerelo@gmail.com>
2015-08-01 21:25:14 +0200

    New beautiful graphs

--- data/time-vs-IPs.R
+++ data/time-vs-IPs.R
@@ -1,7 +1,11 @@
 library(ggplot2)
 
 ips.time <- read.csv("ips-time.csv")
+ips.time$howmany <- 1:nrow(ips.time)
 for ( i in 1:nrow(ips.time) ) {
     ips.time[i,]$howmany <- length(ips.time[ips.time$IPs==ips.time[i,]$IPs,]$milliseconds)
 }
+ips.time$i <- 1:nrow(ips.time)
 ggplot(data=ips.time, aes(x=IPs,y=milliseconds,color=howmany))+geom_point()+scale_y_log10()+ stat_summary(fun.data = "mean_cl_boot", colour = "red")
+ggplot(data=ips.time, aes(x=i,y=milliseconds,color=IPs))+geom_point()+scale_y_log10() + scale_color_gradient(low='blue', high='yellow')
+
