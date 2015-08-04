a716186e15319c9f97c1fd2c3068f5b7c07ee645
JJ Merelo <jjmerelo@gmail.com>
2015-08-04 18:47:02 +0200

    Updated data and representation

--- data/time-vs-IPs-openshift.R
+++ data/time-vs-IPs-openshift.R
@@ -7,5 +7,6 @@ for ( i in 1:nrow(ips.time) ) {
 }
 ips.time$i <- 1:nrow(ips.time)
 ggplot(data=ips.time, aes(x=IPs,y=milliseconds,color=howmany))+geom_point()+scale_y_log10()+ stat_summary(fun.data = "mean_cl_boot", colour = "red")
+ggplot(data=ips.time, aes(x=i,y=milliseconds,fill=IPs))+geom_bar(stat='identity')+scale_y_log10()
 ggplot(data=ips.time, aes(x=i,y=milliseconds,color=IPs))+geom_point()+scale_y_log10() + scale_color_gradient(low='blue', high='yellow')
 
