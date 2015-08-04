e452a716fb9bdd7716888962e25ddece168cce0d
JJ Merelo <jjmerelo@gmail.com>
2015-08-04 19:23:50 +0200

    Answering @pacastillo questions

--- data/time-vs-IPs-openshift.R
+++ data/time-vs-IPs-openshift.R
@@ -9,4 +9,5 @@ ips.time$i <- 1:nrow(ips.time)
 ggplot(data=ips.time, aes(x=IPs,y=milliseconds,color=howmany))+geom_point()+scale_y_log10()+ stat_summary(fun.data = "mean_cl_boot", colour = "red")
 ggplot(data=ips.time, aes(x=i,y=milliseconds,fill=IPs))+geom_bar(stat='identity')+scale_y_log10()
 ggplot(data=ips.time, aes(x=i,y=milliseconds,color=IPs))+geom_point()+scale_y_log10() + scale_color_gradient(low='blue', high='yellow')
+ggplot(data=ips.time, aes(x=reorder(i,-milliseconds),y=milliseconds,color=IPs)) +geom_point()+scale_y_log10() + scale_color_gradient(low='blue', high='yellow')+theme(axis.ticks = element_blank(), axis.text.x = element_blank(),axis.text.x=element_text("Ordered experiments"))
 
