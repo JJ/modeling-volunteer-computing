1c7c728df2b11b8999f01e8589064710959a3e5f
JJ Merelo <jjmerelo@gmail.com>
2015-08-09 12:34:08 +0200

    Adding explanation for WW
    Please @mariosky @fergunet @pacastillo check the conclusions. This is almost done now, I'll write the conclusions soon.

--- data/ips-puts.R
+++ data/ips-puts.R
@@ -1,5 +1,13 @@
-load(ggplot2)
+library(ggplot2)
 library(evd)
 ips.puts.ww <- read.csv("ips-puts-ww.dat")
+ips.puts.ww.df <- data.frame(x=1:nrow(ips.puts.ww),
+                             puts=sort(ips.puts.ww$puts,decreasing=T))
 this.fit <- fgev(ips.puts.ww$puts)
 print(this.fit)
+this.fit.plot <- data.frame(x=1:length(ips.puts.ww$puts),
+                            y=sort(rgev(length(ips.puts.ww$puts),
+                                this.fit$estimate["loc"],
+                                this.fit$estimate["scale"],
+                                this.fit$estimate["shape"]),decreasing=T))
+ggplot()+geom_point(data=ips.puts.ww.df,aes(x=x,y=puts,color='Data'))+geom_point(data=this.fit.plot,aes(x=x,y=y,color='Fit'))+scale_y_log10()
