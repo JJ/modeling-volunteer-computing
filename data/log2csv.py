d00894f9faddc2e7d7d4794ddae20baf00814598
psicobyte <psicobyte@gmail.com>
2015-10-23 16:39:58 +0200

    Controlando los argumentos de entrada

--- data/log2csv.py
+++ data/log2csv.py
@@ -3,42 +3,48 @@
 
 import ast, sys
 
-ruta_log = sys.argv[1]
-ruta_csv = sys.argv[2]
+def procesa(input_file, output_file):
 
-lista_lineas = list()
+    ruta_log = input_file
+    ruta_csv = output_file
 
-#Abrimos el fichero en modo lectura
-fichero = open(ruta_log,"r")
+    lista_lineas = list()
 
+    #Abrimos el fichero en modo lectura
+    fichero = open(ruta_log,"r")
 
+    for linea in fichero:
 
-for linea in fichero:
-
-    lista_lineas.append(ast.literal_eval(linea))
-
-
+        lista_lineas.append(ast.literal_eval(linea))
 
 
-fichero_salida = open(ruta_csv,"w")
+    fichero_salida = open(ruta_csv,"w")
 
-elemento = '"chromosome","fitness","IP","date","time"\n'
-fichero_salida.write(elemento)
+    elemento = '"chromosome","fitness","IP","date","time"\n'
+    fichero_salida.write(elemento)
 
 
-for diccionario in lista_lineas:
+    for diccionario in lista_lineas:
 
-    try:
+        try:
  
-        fh1, fh2 = diccionario["timestamp"].split("T")
-        fh = '"' + fh1 + '","' + fh2 + '"'
+            fh1, fh2 = diccionario["timestamp"].split("T")
+            fh = '"' + fh1 + '","' + fh2 + '"'
 
-        elemento = '"' + str(diccionario["chromosome"]) + '","' + str(diccionario["fitness"]) + '","' + str(diccionario["IP"]) + '","' + fh + '"\n'
-        fichero_salida.write(elemento)
-    except:
-        pass
+            elemento = '"' + str(diccionario["chromosome"]) + '","' + str(diccionario["fitness"]) + '","' + str(diccionario["IP"]) + '","' + fh + '"\n'
+            fichero_salida.write(elemento)
+        except:
+            pass
 
 
 
-fichero.close()
-fichero_salida.close()
+    fichero.close()
+    fichero_salida.close()
+
+
+if len(sys.argv) > 2:
+
+    procesa(sys.argv[1],sys.argv[2])
+
+else:
+    print "Usage: log2.csv input_file output_file"
