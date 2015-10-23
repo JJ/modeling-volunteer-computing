ec572f91952cd0d26255e21f51c2a956bf5b70d9
psicobyte <psicobyte@gmail.com>
2015-10-23 17:12:32 +0200

    Agregados docstrings

--- data/log2csv.py
+++ data/log2csv.py
@@ -1,28 +1,37 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
+"""
+Script parq convertir los ficheros *.log en *.csv.
+
+Sólo extrae las filas con campo "chromosome", y sólo parte de los campos (Los que incho necesita)
+"""
 
 import ast, sys
 
 def procesa(input_file, output_file):
+"""
+El primer argumento es El archivo *.log de entrada, el segundo el *.csv de salida
+"""
+
 
     ruta_log = input_file
     ruta_csv = output_file
 
     lista_lineas = list()
 
-    #Abrimos el fichero en modo lectura
+    #Abrimos el fichero de entrada en modo lectura
     fichero = open(ruta_log,"r")
 
     for linea in fichero:
 
         lista_lineas.append(ast.literal_eval(linea))
 
-
+    #Abrimos el fichero de salida en modo escritura
     fichero_salida = open(ruta_csv,"w")
 
+    # Cabecera del CSV
     elemento = '"chromosome","fitness","IP","date","time"\n'
     fichero_salida.write(elemento)
-
 
     for diccionario in lista_lineas:
 
@@ -36,11 +45,8 @@ def procesa(input_file, output_file):
         except:
             pass
 
-
-
     fichero.close()
     fichero_salida.close()
-
 
 if len(sys.argv) > 2:
 
