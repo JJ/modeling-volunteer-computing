ef325736ef0aeb1f68c1d8b6f9954f42a23e22fa
JJ Merelo <jjmerelo@gmail.com>
2015-11-17 18:07:42 +0100

    Working on Rastrigin Fitness

---
+++ data/plot-rastrigin-fitness.R
@@ -0,0 +1,7 @@
+library(ggplot2)
+library(scales)
+
+rastrigin.fitness <- read.csv("rastrigin-fitness.csv")
+rastrigin.fitness$date <-  as.POSIXct(rastrigin.fitness$timestamp, "%Y-%m-%d %H:%M:%S")
+qplot(date,-Fitness,data=rastrigin.fitness)+ scale_x_datetime()
+ggsave("rastrigin-fitness.png",width=4,height=3)
