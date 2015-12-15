611e431d21a87ea8aa82de0ce5f993a9d23932e1
JJ Merelo <jjmerelo@gmail.com>
2015-12-15 19:09:11 +0100

    Changes fig 8 refs #70

--- data/ips-puts-rastrigin.R
+++ data/ips-puts-rastrigin.R
@@ -13,9 +13,9 @@ this.fit.plot <- data.frame(rank=1:length(ips.puts.ww.rastrigin$puts),
                                 this.fit$estimate["scale"],
                                 this.fit$estimate["shape"]),decreasing=T))
 
-ggplot() +geom_point(data=ips.puts.ww.rastrigin.df,aes(x=rank,y=puts,color='Data',shape='Data')) +geom_point(data=this.fit.plot,aes(x=rank,y=y,color='Fit',shape='Fit'))+scale_y_log10() + scale_colour_discrete(name  ="Puts", breaks=c("Data", "Fit"), labels=c("Data", "Fit")) + scale_shape_discrete(name  ="Puts", breaks=c("Data", "Fit"), labels=c("Data", "Fit"))
+ggplot() +geom_point(data=ips.puts.ww.rastrigin.df,aes(x=rank,y=puts,color='Data',shape='Data')) +geom_point(data=this.fit.plot,aes(x=rank,y=y,color='Fit',shape='Fit'))+scale_y_log10() + scale_colour_grey(name  ="Puts", breaks=c("Data", "Fit"), labels=c("Data", "Fit")) + scale_shape_discrete(name  ="Puts", breaks=c("Data", "Fit"), labels=c("Data", "Fit"))
 
-ggsave('gev-fit-ww.rastrigin.png',width=5,height=5)
+ggsave('../img/gev-fit-ww.rastrigin.png',width=5,height=5)
 
 print(fitdistr(ips.puts.ww.rastrigin$puts,"gamma"))
 this.weibull <- fitdistr(ips.puts.ww.rastrigin$puts,"weibull")
@@ -24,8 +24,8 @@ this.fit.weib <- data.frame(rank=1:length(ips.puts.ww.rastrigin$puts),
 y=sort(rweibull(length(ips.puts.ww.rastrigin$puts),
     this.weibull$estimate["shape"],
     this.weibull$estimate["scale"]),decreasing=T))
-ggplot()+geom_point(data=ips.puts.ww.rastrigin.df,aes(x=rank,y=puts,color='Data',shape='Data'))+geom_point(data=this.fit.weib,aes(x=rank,y=y,color='Fit',shape='Fit'))+scale_y_log10()+ scale_colour_discrete(name ="Puts", breaks=c("Data", "Fit"), labels=c("Data", "Fit")) + scale_shape_discrete(name ="Puts", breaks=c("Data", "Fit"), labels=c("Data", "Fit"))
-ggsave('weibull-fit-ww.rastrigin.png',width=5,height=5)
+ggplot()+geom_point(data=ips.puts.ww.rastrigin.df,aes(x=rank,y=puts,color='Data',shape='Data'))+geom_point(data=this.fit.weib,aes(x=rank,y=y,color='Fit',shape='Fit'))+scale_y_log10()+ scale_colour_grey(name ="Puts", breaks=c("Data", "Fit"), labels=c("Data", "Fit")) + scale_shape_discrete(name ="Puts", breaks=c("Data", "Fit"), labels=c("Data", "Fit"))
+ggsave('../img/weibull-fit-ww.rastrigin.png',width=5,height=5)
 
 ips.puts.ww.rastrigin.workers <- read.csv("IPs-worker-Rastrigin.csv")
 ips.puts.ww.rastrigin.workers.df <- data.frame(rank=1:nrow(ips.puts.ww.rastrigin.workers),
@@ -38,9 +38,9 @@ this.fit.plot <- data.frame(rank=1:length(ips.puts.ww.rastrigin.workers$puts),
                                 this.fit$estimate["scale"],
                                 this.fit$estimate["shape"]),decreasing=T))
 
-ggplot() +geom_point(data=ips.puts.ww.rastrigin.workers.df,aes(x=rank,y=puts,color='Data',shape='Data')) +geom_point(data=this.fit.plot,aes(x=rank,y=y,color='Fit',shape='Fit'))+scale_y_log10() + scale_colour_discrete(name  ="Puts", breaks=c("Data", "Fit"), labels=c("Data", "Fit")) + scale_shape_discrete(name  ="Puts", breaks=c("Data", "Fit"), labels=c("Data", "Fit"))
+ggplot() +geom_point(data=ips.puts.ww.rastrigin.workers.df,aes(x=rank,y=puts,color='Data',shape='Data')) +geom_point(data=this.fit.plot,aes(x=rank,y=y,color='Fit',shape='Fit'))+scale_y_log10() + scale_colour_grey(name  ="Puts", breaks=c("Data", "Fit"), labels=c("Data", "Fit")) + scale_shape_discrete(name  ="Puts", breaks=c("Data", "Fit"), labels=c("Data", "Fit"))
 
-ggsave('gev-fit-ww.rastrigin.workers.png',width=5,height=5)
+ggsave('../img/gev-fit-ww.rastrigin.workers.png',width=5,height=5)
 
 print(fitdistr(ips.puts.ww.rastrigin.workers$puts,"gamma"))
 this.weibull <- fitdistr(ips.puts.ww.rastrigin.workers$puts,"weibull")
@@ -50,5 +50,5 @@ y=sort(rweibull(length(ips.puts.ww.rastrigin.workers$puts),
     this.weibull$estimate["shape"],
     this.weibull$estimate["scale"]),decreasing=T))
 print(this.weibull)
-ggplot()+geom_point(data=ips.puts.ww.rastrigin.workers.df,aes(x=rank,y=puts,color='Data',shape='Data'))+geom_point(data=this.fit.weib,aes(x=rank,y=y,color='Fit',shape='Fit'))+scale_y_log10()+ scale_colour_discrete(name ="Puts", breaks=c("Data", "Fit"), labels=c("Data", "Fit")) + scale_shape_discrete(name ="Puts", breaks=c("Data", "Fit"), labels=c("Data", "Fit"))
-ggsave('weibull-fit-ww.rastrigin.workers.png',width=5,height=5)
+ggplot()+geom_point(data=ips.puts.ww.rastrigin.workers.df,aes(x=rank,y=puts,color='Data',shape='Data'))+geom_point(data=this.fit.weib,aes(x=rank,y=y,color='Fit',shape='Fit'))+scale_y_log10()+ scale_colour_grey(name ="Puts", breaks=c("Data", "Fit"), labels=c("Data", "Fit")) + scale_shape_discrete(name ="Puts", breaks=c("Data", "Fit"), labels=c("Data", "Fit"))
+ggsave('../img/weibull-fit-ww.rastrigin.workers.png',width=5,height=5)
