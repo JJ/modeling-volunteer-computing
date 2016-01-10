ee094e3fe511713a9f2208bdaa7f9cae55832eef
JJ Merelo <jjmerelo@gmail.com>
2016-01-10 19:26:11 +0100

    Better fig 4 refs #89

--- data/time-vs-IPs.R
+++ data/time-vs-IPs.R
@@ -6,7 +6,7 @@ for ( i in 1:nrow(ips.time) ) {
     ips.time[i,]$howmany <- length(ips.time[ips.time$IPs==ips.time[i,]$IPs,]$milliseconds)
 }
 ips.time$i <- 1:nrow(ips.time)
-ggplot(data=ips.time, aes(x=IPs,y=milliseconds,color=howmany))+geom_point()+scale_y_log10()+ scale_colour_gradient(name  ="Count", low="white",high='gray70')+ stat_summary(fun.data = "mean_cl_boot", colour = "black")
+ggplot(data=ips.time, aes(x=IPs,y=milliseconds,color=howmany))+geom_point()+scale_y_log10()+ scale_colour_gradient(name  ="Count", low="#11AA99",high='#EE3333')+ stat_summary(fun.data = "mean_cl_boot", colour = "black")
 ggsave("../img/ips-time-ww.png", width=5,height=5)
 ggplot(data=ips.time, aes(x=i,y=milliseconds,color=IPs))+geom_point()+scale_y_log10() + scale_color_gradient(low='blue', high='yellow')
 
